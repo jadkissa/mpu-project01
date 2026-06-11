569e5109d46ca73e5f365af6b0f6417f4d67a6e3
jadkissa <jadissa93@gmail.com>
2026-06-12 01:26:27 +0300

    modify: snort config and rules

--- snort/snort.lua
+++ snort/snort.lua
@@ -1,27 +1,55 @@
--- rules
+HOME_NET = 'any'
+EXTERNAL_NET = 'any'
+HTTP_PORTS = '8080 9090'
+
+include '/home/snorty/snort3/etc/snort/snort_defaults.lua'
+
+network = {
+    checksum_drop = 'none',
+    checksum_eval = 'none',
+}
+
+-- TCP reassembly required for flow:established and HTTP payload inspection
+stream = {}
+stream_ip = {}
+stream_icmp = {}
+stream_tcp = {}
+stream_udp = {}
+
+http_inspect = {}
+
+wizard = default_wizard
+
+binder = {
+    { when = { service = 'http' }, use = { type = 'http_inspect' } },
+    { use = { type = 'wizard' } },
+}
+
 ips = {
     include = '/etc/snort/rules/local.rules',
     variables = {
         nets = {
-            HOME_NET = '172.20.10.0/28',
-            EXTERNAL_NET = '!$HOME_NET',
+            HOME_NET = HOME_NET,
+            EXTERNAL_NET = EXTERNAL_NET,
         },
         ports = {
-            HTTP_PORTS = '8080',
-        }
-    }
+            HTTP_PORTS = HTTP_PORTS,
+        },
+    },
 }
 
--- detection engine
 detection = {
     hyperscan_literals = false,
 }
 
--- logging
 alert_json = {
     file = true,
     limit = 10,
     fields = 'timestamp src_addr dst_addr src_port dst_port proto gid sid rev msg priority class',
 }
 
-http_inspect = {}
+daq = {
+    modules = {
+        { name = 'pcap' },
+    },
+}
